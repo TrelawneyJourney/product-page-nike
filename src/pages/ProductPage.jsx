b8a9d0bebbab199b060ee210210d6107be6ad6f2
TrelawneyJourney <the-trelawney-journey@outlook.com>
2025-09-12 10:39:41 -0300

    add util assets url

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,6 +1,7 @@
 import SizeSelector from "../components/SizeSelector";
 import ColorSelector from "../components/ColorSelector";
 import { useEffect, useState } from "react";
+import { getAssetUrl } from "../utils/getAssetUrl";
 
 export default function ProductPage() {
   const [product, setProduct] = useState(null);
@@ -13,10 +14,14 @@ export default function ProductPage() {
     }).format(precio);
 
   useEffect(() => {
-    fetch("/data/products.json")
+    fetch(`${import.meta.env.BASE_URL}data/products.json`)
       .then((res) => res.json())
-      .then((data) => setProduct(data[0]));
+      .then((data) => setProduct(data[0]))
+      .catch((err) => console.error("Error al cargar JSON:", err));
   }, []);
+
+  if (!product)
+    return <p className="text-neutral-500 text-center p-6">Cargando...</p>;
 
   return (
     <div className="layout-product-page p-12">
@@ -27,13 +32,26 @@ export default function ProductPage() {
             <p className="font-bold text-sm uppercase">{product.marca}</p>
             <h1 className="text-xl">{product.name}</h1>
             <p className="text-neutral-500">{product.descripcion}</p>
-            <div className="flex">imagenes chicas</div>
+            <div className="flex gap-1 py-4">
+              {product.img.negro.map((img, i) => (
+                <img
+                  key={i}
+                  src={getAssetUrl(img)}
+                  alt={product.name}
+                  className="w-[100px]"
+                />
+              ))}
+            </div>
           </div>
 
           {/**col-2 */}
           <div className="overflow-hidden">
             <div className="flex justify-center items-center w-full h-auto">
-              <img src={product.img.negro[0]} alt="" className="w-[500px]" />
+              <img
+                src={getAssetUrl(product.img.negro[0])}
+                alt={product.name}
+                className="w-[500px]"
+              />
             </div>
           </div>
 
