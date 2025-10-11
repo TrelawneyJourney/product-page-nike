9b2ebc43f253290a922cd1add6525a9499957abf
TrelawneyJourney <the-trelawney-journey@outlook.com>
2025-10-11 12:40:37 -0300

    add accordionMenu

---
+++ src/components/AccordionItem.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export default function AccordionItem({ title, links }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="border-b border-gray-200">
+      <button
+        className="
+            w-full flex justify-between items-center py-3 text-lg font-semibold"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {title}
+        <span>{isOpen ? "-" : "+"}</span>
+      </button>
+
+      <div
+        className={`overflow-hidden transition-all duration-300 ${
+          isOpen ? "max-h-40" : "max-h-0"
+        }`}
+      >
+        <ul className="flex flex-col gap-2 pl-4 pb-2">
+          {links.map((link) => (
+            <li key={link} className="text-sm text-gray-700">
+              {link}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,72 +1,11 @@
-import { IoMenu } from "react-icons/io5";
-import SearchBar from "./SearchBar";
 import TopBar from "./TopBar";
-import { menu, menuCompleto } from "../data/nav";
-import { useState } from "react";
-import NikeSvg from "../assets/svg/NikeSvg";
-import MenuIcons from "./MenuIcons";
+import NavBar from "./NavBar";
 
 export default function Header() {
-  const [menuActive, setMenuActive] = useState(null);
-
   return (
-    <div
-      className="flex flex-col relative"
-      onMouseLeave={() => setMenuActive(null)}
-    >
+    <div className="flex flex-col">
       <TopBar />
-
-      <nav className="w-full flex justify-between items-center px-4 md:px-10 ">
-        <div className="logo">
-          <a href="">
-            <NikeSvg />
-          </a>
-        </div>
-
-        {/* menu  */}
-        <ul className="hidden md:flex">
-          {menu.map((item) => (
-            <li
-              key={item}
-              onMouseEnter={() => setMenuActive(item)}
-              className="px-3 py-1 font-semibold hover:underline hover:decoration-2 hover:underline-offset-4 cursor-pointer"
-            >
-              {item}
-            </li>
-          ))}
-        </ul>
-
-        <div className="flex items-center gap-2">
-          <SearchBar />
-          <MenuIcons />
-          <IoMenu className="md:hidden nav-items" />
-        </div>
-      </nav>
-
-      {menuActive && menuCompleto[menuActive] && (
-        <div className="absolute left-0 top-full w-full bg-white shadow-lg flex justify-center py-6 z-50">
-          <div className="flex gap-8 max-w-6xl w-full justify-center">
-            {Object.entries(menuCompleto[menuActive]).map(([cat, links]) => (
-              <div
-                key={cat}
-                className="text-sm flex flex-col basis-[180px] max-w-[220px] shrink-0"
-              >
-                <h3 className="font-semibold mb-2">{cat}</h3>
-                <ul className="">
-                  {links.map((link) => (
-                    <li
-                      key={link}
-                      className="text-gray-600 hover:text-black mb-2 leading-snug break-words whitespace-normal cursor-pointer"
-                    >
-                      {link}
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ))}
-          </div>
-        </div>
-      )}
+      <NavBar />
     </div>
   );
 }
